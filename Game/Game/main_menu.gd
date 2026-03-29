09e18c1740c71102e849d11a84a46405b236047b
unknown <KIfoul21@gmail.com>
2026-03-29 16:48:40 -0400

    b

--- Game/Game/main_menu.gd
+++ Game/Game/main_menu.gd
@@ -1,21 +1,18 @@
 class_name MainMenu extends Control
 
-
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS  # works even when paused
 	$TutoMenu.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().paused = false
 	$".".visible = false
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_how_to_play_pressed() -> void:
 	$TutoMenu.visible = true
-
 
 func _on_button_pressed() -> void:
 	$TutoMenu.visible = false
